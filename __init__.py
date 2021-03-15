885ce3e32769a2bbc323145056ce9f1eb29d429d
DaniloToroL <danilo.toro@usach.cl>
2021-03-15 11:41:10 -0300

    add close command and session

--- __init__.py
+++ __init__.py
@@ -3,20 +3,28 @@ import json
 from subprocess import Popen, PIPE
 import sys
 
-global cred_oracle
 base_path = tmp_global_obj["basepath"]
-curpath = base_path  + "modules" + os.sep + "Oracle" + os.sep + "libs"
+cur_path = base_path + "modules" + os.sep + "Oracle" + os.sep + "libs"
 bin_path = base_path + "modules" + os.sep + "Oracle" + os.sep + "bin"
 
 if bin_path not in os.environ['PATH']:
     os.environ["PATH"] += os.pathsep + bin_path
 
-if curpath not in sys.path:
-    sys.path.append(curpath)
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
 import cx_Oracle
 
-global con
-global cursor
+# Globals declared here
+global mod_oracle_sessions
+# Default declared here
+SESSION_DEFAULT = "default"
+# Initialize settings for the module here
+try:
+    if not mod_oracle_sessions:
+        mod_oracle_sessions = {SESSION_DEFAULT: {}}
+except NameError:
+    mod_oracle_sessions = {SESSION_DEFAULT: {}}
 
 
 module = GetParams('module')
@@ -25,45 +33,64 @@ if module == "connect":
     user = GetParams('user')
     password = GetParams('password')
     dsn = GetParams('dsn')
+    session = GetParams('session')
     result = GetParams('result')
-    #process = "oracle.exe {user} {password} {dsn} \"select name from v$database\"".format(user=user, password=password,
-                                                                                          #dsn=dsn)
-    #con = Popen(process, env=os.environ.copy(), shell=True, stdout=PIPE, stderr=PIPE)
+
+    if not session:
+        session = SESSION_DEFAULT
+
     con = cx_Oracle.connect(user, password, dsn)
     try:
         cursor = con.cursor()
-        res = True
-        SetVar(result, res)
+        mod_oracle_sessions[session] = {
+            "connection": con,
+            "cursor": cursor
+        }
+        SetVar(result, True)
     except Exception as e:
+        SetVar(result, False)
         PrintException()
         raise e
 
 if module == "execute":
     query = GetParams('query')
+    session = GetParams('session')
     result = GetParams('result')
 
-    # user = cred_oracle['user']
-    # password = cred_oracle['password']
-    # dsn = cred_oracle['dsn']
-    # # print("ddd", cred_oracle)
-    # process = "oracle.exe {user} {password} {dsn} \"{query}\"".format(user=user, password=password,
-    #                                                                   dsn=dsn, query=query)
+    if not session:
+        session = SESSION_DEFAULT
 
-    # print('ddd', process)
-    # con = Popen(process, env=os.environ.copy(), shell=True, stdout=PIPE, stderr=PIPE)
+    cursor = mod_oracle_sessions[session]["cursor"]
+    con = mod_oracle_sessions[session]["connection"]
 
     try:
-       cursor.execute(query)
-       if query.lower().startswith("insert") or query.lower().startswith("update") or query.lower().startswith(
-               "delete") or query.lower().startswith("alter"):
-           con.commit()
-           if result:
-               SetVar(result, True)
+        cursor.execute(query)
+        if query.lower().startswith(("insert", "update", "delete", "alter")):
+            con.commit()
+            if result:
+                SetVar(result, True)
         # cred_oracle = {"user": user, "password": password, "dsn": dsn}
-       else:
-           data = [r for r in cursor]
-           if result:
-               SetVar(result, data)
+        else:
+            data = [r for r in cursor]
+            if result:
+                SetVar(result, data)
     except Exception as e:
         PrintException()
         raise e
+
+if module == "close":
+    session = GetParams('session')
+
+    if not session:
+        session = SESSION_DEFAULT
+
+    cursor = mod_oracle_sessions[session]["cursor"]
+    con = mod_oracle_sessions[session]["connection"]
+
+    try:
+        cursor.close()
+        con.close()
+    except Exception as e:
+        PrintException()
+        raise e
+
